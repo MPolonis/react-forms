566094e4a352c125626beab561bfd0eabf5a4261
Marcin Polonis <marcin.polonis@o2.pl>
2020-08-03 16:21:48 +0200

    Start creating forms with react

---
+++ forms/src/Editor.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export class Editor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ""
+        }
+    }
+
+    updateFormValue = (e) => {
+        this.setState({[e.target.name]: e.target.value},
+            () => this.props.submit(this.state));
+    }
+
+    render() {
+        return (
+            <div className="h5 bg-info text-white p-2">
+                <div className="form-group">
+                    <label>Imię</label>
+                    <input className="form-control"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.updateFormValue} />
+                </div>
+            </div>
+        )
+    }
+}
